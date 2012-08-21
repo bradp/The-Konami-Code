1c6075957b0d23ba8474182510bb19e1758cc989
Aaron Holbrook <aaron@a7web.com>
2012-08-20 21:42:42 -0500

    added plugin core file and description

---
+++ the-konami-code.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ Plugin Name: The Konami Code
+ Plugin URI: 
+ Description: Easily add easter eggs to your site, with class
+ Author: AaronHolbrook and BradParbs
+ Version: 1.0
+ Author URI: 
+ */
+
