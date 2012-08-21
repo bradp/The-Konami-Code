3156bd0870891e1acbc9d5d915e235e33ff1d6e4
Aaron Holbrook <aaron@a7web.com>
2012-08-20 21:46:57 -0500

    added framework for plugin

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 The-Konami-Code
 ===============
-
 Everything old is new again. Easily add easter eggs to your WordPress site with this plugin!
+
+The Benefits
+============
+* Benefit list
+
+Installation
+============
+Installation instructions
+
+Templates
+=========
--- the-konami-code.php
+++ the-konami-code.php
@@ -8,3 +8,54 @@
  Author URI: 
  */
 
+
+// Derive the current path and load up Sanity
+$plugin_path = dirname(__FILE__).'/';
+if(class_exists('SanityPluginFramework') != true)
+    require_once($plugin_path.'framework/sanity.php');
+
+
+/*
+*   Define your plugin class which extends the SanityPluginFramework
+*   Make sure you skip down to the end of this file, as there are a few
+*   lines of code that are very important.
+*/ 
+class ExamplePlugin extends SanityPluginFramework {
+  
+  /*
+  * Some required plugin information
+  */
+  var $version = '1.0';
+  
+  /*
+  *   Required __construct() function that initalizes the Sanity Framework
+  */
+  function __construct() {
+      parent::__construct(__FILE__);
+  }
+
+  /*
+  *   Run during the activation of the plugin
+  */
+  function activate() {
+    
+  }
+  
+  /*
+  *   Run during the initialization of Wordpress
+  */
+  function initialize() {
+  
+  }
+  
+}
+
+// Initalize the your plugin
+$ExamplePlugin = new ExamplePlugin();
+
+// Add an activation hook
+register_activation_hook(__FILE__, array(&$ExamplePlugin, 'activate'));
+
+// Run the plugins initialization method
+add_action('init', array(&$ExamplePlugin, 'initialize'));
+
